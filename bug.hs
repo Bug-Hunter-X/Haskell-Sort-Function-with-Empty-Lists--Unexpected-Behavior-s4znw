9e693e84f02d6042c23313a7a8d0966c182a8106
BugHunter-X <askzenai@gmail.com>
2025-01-23 03:44:09 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,30 @@
+```haskell
+import Data.List (sort)
+
+main :: IO ()
+main = do
+  let xs = [1, 2, 3, 4, 5]
+  let ys = sort xs
+  print ys -- this will print [1,2,3,4,5]
+  print $ length ys -- this will print 5
+
+  let zs = [1, 2, 3, 4, 5] ++ [1,2]
+  let ws = sort zs
+  print ws --this will print [1,1,2,2,3,4,5]
+  print $ length ws --this will print 6
+
+  let as = [1,2,3,4,5] ++ []
+  let bs = sort as
+  print bs --this will print [1,2,3,4,5]
+  print $ length bs --this will print 5
+
+  let cs = [] ++ [1,2,3,4,5]
+  let ds = sort cs
+  print ds --this will print [1,2,3,4,5]
+  print $ length ds --this will print 5
+
+  let es = [] ++ []
+  let fs = sort es
+  print fs --this will print []
+  print $ length fs --this will print 0
+```
